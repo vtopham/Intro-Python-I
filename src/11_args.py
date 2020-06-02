c689fa7abb4b708fb1e201dbd43cbc079ee29ac4
Victoria  Topham <topham.victoria@gmail.com>
2020-06-02 08:02:51 -0700

    11_args complete

--- src/11_args.py
+++ src/11_args.py
@@ -5,7 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
-
+def f1(num1, num2):
+    return num1 + num2
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
@@ -13,6 +14,13 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*nums):
+    sum = 0
+    if isinstance(nums[0], list):
+        nums = nums[0]
+    for num in nums:
+        sum += num
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -30,7 +38,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
-
+def f3(num1, num2 = 1):
+    return num1 + num2
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -44,6 +53,18 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**args):
+    
+    #one way to do it... lots more code
+        # x = [arg for arg in args]
+        # y = [arg for arg in args.values()]
+        # for i in range(len(x)):
+        #     print(f"key: {x[i]}, value: {y[i]}")
+
+    
+    print({f"key: {k}, value: {v}" for (k,v) in args.items()})
+        
+        
 
 # Should print
 # key: a, value: 12
@@ -62,4 +83,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
